8d99fd63f41b202146813adb8c51ccb6a2a8c51a
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-10-29 22:41:42 -0200

    Buildings generating money and taking time to be built

--- Assets/Scripts/Buildings/BuildingController.cs
+++ Assets/Scripts/Buildings/BuildingController.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingController : MonoBehaviour {
 
 	public float timeToGenerateCoins = 30f;
+	public float timeToBuild = 15f;
 	public int income = 10;
 	public int cost = 1;
 	public SpriteRenderer cantBuildImage;
+	public Slider buildingProgressBar;
 
 	private bool canBuild;
+	private IEnumerator routine;
+	private float timeCount;
 
 	void Awake()
 	{
 		cantBuildImage.enabled = false;
+		buildingProgressBar.gameObject.SetActive (false);
 		canBuild = true;
 	}
 
@@ -41,10 +47,55 @@ public class BuildingController : MonoBehaviour {
 		{
 			cantBuildImage.gameObject.SetActive (false);
 			GameController.instance.Spend (cost);
+			timeCount = 0f;
+
+			routine = Constructing();
+
+			StartCoroutine (routine);
 		}
 		else
 		{
 			Destroy (gameObject);
+		}
+	}
+
+	private IEnumerator Constructing()
+	{
+		buildingProgressBar.gameObject.SetActive (true);
+
+		while(timeCount < timeToBuild)
+		{
+			timeCount += Time.deltaTime;
+
+			buildingProgressBar.value = timeCount / timeToBuild;
+
+			yield return null;
+
+		}
+
+		buildingProgressBar.gameObject.SetActive (false);
+
+		routine = GenerateMoney ();
+		StartCoroutine (routine);
+	}
+
+	private IEnumerator GenerateMoney()
+	{
+		yield return null;
+
+		while(true)
+		{
+			timeCount += Time.deltaTime;
+
+			if(timeCount > timeToGenerateCoins)
+			{
+				timeCount -= timeToGenerateCoins;
+				GameController.instance.Receive (income);
+				Debug.Log ("Receive: " + income);
+
+			}
+
+			yield return null;
 		}
 	}
 }
