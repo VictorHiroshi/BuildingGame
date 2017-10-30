cd369ca2f38e2985ed35d79c2f9d3781735ac31d
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-10-29 23:40:16 -0200

    Change sprite of pause/play button

---
+++ Assets/Scripts/Miscellaneous/ButtonSpriteChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSpriteChange : MonoBehaviour {
+
+	public Image buttonImage;
+
+	public Sprite spriteNotClicked;
+	public Sprite spriteClicked;
+
+	private bool clicked = false;
+
+	public void Click()
+	{
+		clicked = !clicked;
+
+		if(clicked)
+		{
+			buttonImage.sprite = spriteClicked;
+		}
+		else
+		{
+			buttonImage.sprite = spriteNotClicked;
+		}
+	}
+
+}
