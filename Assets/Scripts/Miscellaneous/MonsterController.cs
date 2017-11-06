bfed888ea58db15db22fb7870190f5c047f91b0b
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-11-06 11:05:44 -0200

    Moving monster around the screen

--- Assets/Scripts/Miscellaneous/MonsterController.cs
+++ Assets/Scripts/Miscellaneous/MonsterController.cs
@@ -1,10 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class MonsterController : MonoBehaviour {
 
+	public float speed = 3f;
+	public AnimatorController animatorController;
 	public CircleCollider2D mouthCollider;
+	public Boundary movementBoundary;
+
+	private bool moving;
+	private Vector3 target;
+
+	void Start()
+	{
+		moving = false;
+	}
+
+	void Update()
+	{
+		if (moving)
+			return;
+
+		target.x = Random.Range (movementBoundary.xMin, movementBoundary.xMax);
+		target.y = Random.Range (movementBoundary.yMin, movementBoundary.yMax);
+
+		StartCoroutine (Move ());
+
+	}
 
 	public void SetMouthToTrue()
 	{
@@ -14,5 +38,20 @@ public class MonsterController : MonoBehaviour {
 	public void SetMouthToFalse()
 	{
 		mouthCollider.enabled = false;
+	}
+
+	private IEnumerator Move()
+	{
+		moving = true;
+
+		float step;
+		while((transform.position - target).magnitude > float.Epsilon)
+		{
+			step = Time.deltaTime * speed;
+			transform.position = Vector3.MoveTowards (transform.position, target, step);
+			yield return null;
+		}
+
+		moving = false;
 	}
 }
