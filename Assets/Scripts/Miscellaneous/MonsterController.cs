a050ffcd7ffb0945b1d9fa85ec311cc8010ce883
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-11-06 11:19:09 -0200

    Monster hit and check if die

--- Assets/Scripts/Miscellaneous/MonsterController.cs
+++ Assets/Scripts/Miscellaneous/MonsterController.cs
@@ -5,28 +5,37 @@ using UnityEditor.Animations;
 
 public class MonsterController : MonoBehaviour {
 
+	public int hitsToDie = 5;
 	public float speed = 3f;
-	public AnimatorController animatorController;
+	public float timeStopedWhenSatisfied = 1f;
+	public Animator animatorController;
 	public CircleCollider2D mouthCollider;
 	public Boundary movementBoundary;
 
-	private bool moving;
+	private int hits;
+	private bool canMove;
+	private WaitForSeconds satisfiedDelay;
 	private Vector3 target;
+	private IEnumerator movingRoutine;
 
 	void Start()
 	{
-		moving = false;
+		canMove = true;
+		hits = 0;
+		satisfiedDelay = new WaitForSeconds (timeStopedWhenSatisfied);
 	}
 
 	void Update()
 	{
-		if (moving)
-			return;
+		if (canMove) 
+		{
+			target.x = Random.Range (movementBoundary.xMin, movementBoundary.xMax);
+			target.y = Random.Range (movementBoundary.yMin, movementBoundary.yMax);
 
-		target.x = Random.Range (movementBoundary.xMin, movementBoundary.xMax);
-		target.y = Random.Range (movementBoundary.yMin, movementBoundary.yMax);
+			movingRoutine = Move ();
 
-		StartCoroutine (Move ());
+			StartCoroutine (movingRoutine);
+		}
 
 	}
 
@@ -40,9 +49,15 @@ public class MonsterController : MonoBehaviour {
 		mouthCollider.enabled = false;
 	}
 
+	public void Hit()
+	{
+		hits++;
+		StartCoroutine (Satisfied ());
+	}
+
 	private IEnumerator Move()
 	{
-		moving = true;
+		canMove = false;
 
 		float step;
 		while((transform.position - target).magnitude > float.Epsilon)
@@ -52,6 +67,36 @@ public class MonsterController : MonoBehaviour {
 			yield return null;
 		}
 
-		moving = false;
+		canMove = true;
+	}
+
+	private IEnumerator Satisfied()
+	{
+		StopCoroutine (movingRoutine);
+		animatorController.SetTrigger ("Satisfied");
+		canMove = false;
+
+		yield return satisfiedDelay;
+
+		if (!Died ()) 
+		{
+			canMove = true;
+			animatorController.SetTrigger ("Normal");
+		}
+		else
+		{
+			Die ();
+		}
+
+	}
+
+	private bool Died()
+	{
+		return hits >= hitsToDie;
+	}
+
+	private void Die()
+	{
+		Destroy (gameObject);
 	}
 }
