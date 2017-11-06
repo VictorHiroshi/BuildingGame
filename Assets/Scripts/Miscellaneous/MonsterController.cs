4a269f73f1be7a6b34001cf760970fcfc39e5aeb
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-11-06 10:40:33 -0200

    Mouth collider turning on and off

---
+++ Assets/Scripts/Miscellaneous/MonsterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour {
+
+	public CircleCollider2D mouthCollider;
+
+	public void SetMouthToTrue()
+	{
+		mouthCollider.enabled = true;
+	}
+
+	public void SetMouthToFalse()
+	{
+		mouthCollider.enabled = false;
+	}
+}
